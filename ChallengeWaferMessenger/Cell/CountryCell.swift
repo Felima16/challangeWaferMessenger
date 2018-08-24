85a0cc1347a6f24381a23bf200b009786143de49
Fernanda de Lima <fernanda.lima@entelgy.com>
2018-08-24 19:02:54 -0300

    modification swipe to delete

--- ChallengeWaferMessenger/Cell/CountryCell.swift
+++ ChallengeWaferMessenger/Cell/CountryCell.swift
@@ -8,9 +8,71 @@
 
 import UIKit
 
+enum Direction{
+    case Left
+    case Right
+    case Up
+    case Down
+}
+
+
 class CountryCell: UITableViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var cellStackView: UIStackView!
+    
+    let menuWidth: CGFloat = 0.8
+    let percentThreshold: CGFloat = 0.3
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlerPan(pan:)))
+        cellStackView.addGestureRecognizer(pan)
+    }
+    
+    @objc private func handlerPan(pan: UIPanGestureRecognizer){
+        let translation = pan.translation(in: cellStackView)
+        
+        let progress = calculateProgress(translationInView: translation, viewBounds: cellStackView.bounds, direction: .Left)
+        
+        if progress > 0.8 {
+            print("delete cell")
+        }
+        
+        print(translation)
+    }
+    
+
+    private func calculateProgress(translationInView: CGPoint, viewBounds:CGRect, direction: Direction) -> CGFloat{
+        let pointOnAxis:CGFloat
+        let axisLength:CGFloat
+        
+        switch direction {
+        case .Up, .Down:
+            pointOnAxis = translationInView.y
+            axisLength = viewBounds.height
+        case .Left, .Right:
+            pointOnAxis = translationInView.x
+            axisLength = viewBounds.width
+        }
+        
+        let movementOnAxis = pointOnAxis/axisLength
+        let positiveMovementOnAxis:Float
+        let positiveMovementOnAxisPercent:Float
+        
+        switch direction {
+        case .Right,.Down: //positive
+            positiveMovementOnAxis = fmax(Float(movementOnAxis), 0.0)
+            positiveMovementOnAxisPercent = fminf(positiveMovementOnAxis, 1.0)
+            return CGFloat(positiveMovementOnAxisPercent)
+        case .Left, .Up: //negative
+            positiveMovementOnAxis = fminf(Float(movementOnAxis), 0.0)
+            positiveMovementOnAxisPercent = fmax(positiveMovementOnAxis, -1.0)
+            return CGFloat(-positiveMovementOnAxisPercent)
+        }
+    }
 }
+    
+
--- ChallengeWaferMessenger/ViewController/ViewController.swift
+++ ChallengeWaferMessenger/ViewController/ViewController.swift
@@ -17,6 +17,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.automaticallyAdjustsScrollViewInsets = false
+        
+        loadCountry()
+    }
+    
+    private func loadCountry(){
         API.get(success: { (country) in
             self.countries = country
             
@@ -48,74 +55,78 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate{
         return cell
     }
     
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        
-        let delete = UITableViewRowAction(style: .destructive, title: "") { (action, indexPath) in
-            // delete item at indexPath
-            self.countries.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-
-        }
-        
-        delete.setImage(size: CGSize(width: UIScreen.main.bounds.width, height: 60), image: #imageLiteral(resourceName: "bombIcon"), bgColor: .purple)
-
-        return [delete]
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 65
     }
     
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    @available(iOS 11.0, *)
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-
-        let deleteAction = UIContextualAction(style: .normal, title: "") { (action, view, completion) in
-            self.countries.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-            completion(true)
-        }
-
-        deleteAction.image = #imageLiteral(resourceName: "bombIcon")
-        deleteAction.backgroundColor = .purple
-        return UISwipeActionsConfiguration(actions: [deleteAction])
-
-    }
+//    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+//
+//        let delete = UITableViewRowAction(style: .destructive, title: "excluir") { (action, indexPath) in
+//            // delete item at indexPath
+//            self.countries.remove(at: indexPath.row)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//
+//        }
+//
+////        delete.setImage(size: CGSize(width: UIScreen.main.bounds.width, height: 60), image: #imageLiteral(resourceName: "bombIcon"), bgColor: .purple)
+//
+//        return [delete]
+//    }
+//
+//    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//        return true
+//    }
+//
+//    @available(iOS 11.0, *)
+//    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+//
+//        let deleteAction = UIContextualAction(style: .normal, title: "") { (action, view, completion) in
+//            self.countries.remove(at: indexPath.row)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//            completion(true)
+//        }
+//
+//        deleteAction.image = #imageLiteral(resourceName: "bombIcon")
+//        deleteAction.backgroundColor = .purple
+//        return UISwipeActionsConfiguration(actions: [deleteAction])
+//
+//    }
     
 }
 
-extension UITableViewRowAction {
-    
-    func setImage(size: CGSize, image: UIImage, bgColor: UIColor) {
-        
-        // calculate actual size & set title with spaces
-        let defaultTextPadding: CGFloat = 15
-        let defaultAttributes = [ kCTFontAttributeName: UIFont.systemFont(ofSize: 18)]   // system default rowAction text font
-        let oneSpaceWidth = NSString(string: " ").size(withAttributes: defaultAttributes as [NSAttributedStringKey : Any]).width
-        let titleWidth = size.width - defaultTextPadding * 2
-        let numOfSpace = Int(ceil(titleWidth / oneSpaceWidth))
-        
-        let placeHolder = String(repeating: " ", count: numOfSpace)
-        let newWidth = (placeHolder as NSString).size(withAttributes: defaultAttributes as [NSAttributedStringKey : Any]).width + defaultTextPadding * 2
-        let newSize = CGSize(width: newWidth, height: size.height)
-        
-        title = placeHolder
-        
-        // set background with pattern image
-        
-        UIGraphicsBeginImageContextWithOptions(newSize, false, UIScreen.main.nativeScale)
-        
-        let context = UIGraphicsGetCurrentContext()!
-        context.setFillColor(bgColor.cgColor)
-        context.fill(CGRect(origin: .zero, size: newSize))
-        
-        let originX = (newWidth - image.size.width)
-        let originY = (size.height - image.size.height) / 2
-        image.draw(in: CGRect(x: originX, y: originY, width: image.size.width, height: image.size.height))
-        let patternImage = UIGraphicsGetImageFromCurrentImageContext()!
-        
-        UIGraphicsEndImageContext()
-        
-        backgroundColor = UIColor(patternImage: patternImage)
-    }
-}
+//extension UITableViewRowAction {
+//
+//    func setImage(size: CGSize, image: UIImage, bgColor: UIColor) {
+//
+//        // calculate actual size & set title with spaces
+//        let defaultTextPadding: CGFloat = 15
+//        let defaultAttributes = [ kCTFontAttributeName: UIFont.systemFont(ofSize: 18)]   // system default rowAction text font
+//        let oneSpaceWidth = NSString(string: " ").size(withAttributes: defaultAttributes as [NSAttributedStringKey : Any]).width
+//        let titleWidth = size.width - defaultTextPadding * 2
+//        let numOfSpace = Int(ceil(titleWidth / oneSpaceWidth))
+//
+//        let placeHolder = String(repeating: " ", count: numOfSpace)
+//        let newWidth = (placeHolder as NSString).size(withAttributes: defaultAttributes as [NSAttributedStringKey : Any]).width + defaultTextPadding * 2
+//        let newSize = CGSize(width: newWidth, height: size.height)
+//
+//        title = placeHolder
+//
+//        // set background with pattern image
+//
+//        UIGraphicsBeginImageContextWithOptions(newSize, false, UIScreen.main.nativeScale)
+//
+//        let context = UIGraphicsGetCurrentContext()!
+//        context.setFillColor(bgColor.cgColor)
+//        context.fill(CGRect(origin: .zero, size: newSize))
+//
+//        let originX = (newWidth - image.size.width)
+//        let originY = (size.height - image.size.height) / 2
+//        image.draw(in: CGRect(x: originX, y: originY, width: image.size.width, height: image.size.height))
+//        let patternImage = UIGraphicsGetImageFromCurrentImageContext()!
+//
+//        UIGraphicsEndImageContext()
+//
+//        backgroundColor = UIColor(patternImage: patternImage)
+//    }
+//}
 
